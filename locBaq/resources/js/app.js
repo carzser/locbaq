ad75473566b25a7f6e010582c2392c9b64c47776
Fazd <fabioandres2907@hotmail.com>
2019-10-12 22:23:58 -0500

    router and vuetify installed

--- locBaq/resources/js/app.js
+++ locBaq/resources/js/app.js
@@ -1,35 +1,26 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
-
+import Vue from 'vue'
 window.Vue = require('vue');
-import Vuetify from 'vuetify';
+import Vuetify from 'vuetify'
+import MainApp from './components/MainApp.vue';
+import VueRouter from 'vue-router'
+import {routes} from './routes'
+
+
 Vue.use(Vuetify);
-import Vuex from 'vuex';
-Vue.use(Vuex);
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(VueRouter);
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
-    el: '#app',
+    el:'#app',
+    router,
+    vuetify : new Vuetify(),
+    components: {
+        MainApp
+    }
 });
---
+++ locBaq/resources/js/routes.js
@@ -0,0 +1,9 @@
+import login from './components/Login.vue'
+
+export const routes = [
+    {
+        path: '/login',
+        component: login,
+
+    }
+]
